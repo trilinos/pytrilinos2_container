dfde3d91e3052b926670974958604777b7bb240b
Christian Glusa <caglusa@sandia.gov>
2025-04-14 10:02:25 -0600

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+# Trilinos in a container
+
+This repository contains Dockerfiles for two images:
+
+- [`dependencies`](https://github.com/users/trilinos/packages/container/package/dependencies): an image with dependencies for Trilinos/PyTrilinos2
+- [`trilinos`](https://github.com/users/trilinos/packages/container/package/trilinos): an image with pre-installed Trilinos
+
+You can run the second image [online](#runOnBinder).
+Both images can also be [pulled](#pull) from the GitHub Container Registry.
+
+
+## [Running in the cloud](#runOnBinder)
+
+PyTrilinos2 can be run directly for free from the browser on a JupyterHub server hosted at https://mybinder.org/
+
+Just click here to launch a notebook server:
+[![Binder](http://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/trilinos/pytrilinos2_container/binder)
+
+
+## [Running locally](#runningLocally)
+
+You can either pull the images from the GitHub Container Registry or build them yourself.
+
+In either case, please install `podman` or `docker` first.
+
+
+### [Pulling the images](#pull)
+
+To pull the image with PyTrilinos2 pre-installed, run
+```
+podman pull ghcr.io/trilinos/pytrilinos2_container/trilinos:latest
+```
+
+To pull an image with only the dependencies for PyTrilinos2, run
+```
+podman pull ghcr.io/trilinos/pytrilinos2_container/dependencies:latest
+```
+
+### [Running locally](#local)
+
+To locally run a container using the image with pre-installed Trilinos, run
+```
+./containerLauncher.sh -c trilinos
+```
+The JupyterHub server will be available at https://127.0.0.1:9999
+
+In order to build Trilinos locally, run
+```
+./containerLauncher.sh -c dependencies
+```
+This will
+1) Build the image with Trilinos dependencies (unless it has been previously built or pulled).
+2) Clone Trilinos (unless there is a folder trilinos/source already.
+3) Launch a container with source and build directory mapped from host.
+
+Once in the container Trilinos can be configured and built using
+```
+configure_trilinos
+build_trilinos
+```
+The configuration for the build can be found in trilinos-build.cmake
---
+++ containerConfig.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+CONFIG=trilinos
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -c|--config)
+            shift
+            CONFIG="$1"
+            shift # past argument
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            ;;
+    esac
+done
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+if [[ $CONFIG == "dependencies" ]] ; then
+    ##################################################
+    # container name
+    CONTAINER_NAME=pytrilinos2_dependencies
+
+    ##################################################
+    # image
+    CONTAINER_HOME=/root
+
+    IMAGE=ghcr.io/trilinos/pytrilinos2_container/dependencies:latest
+    DOCKERFILE=${SCRIPT_PATH}/Dockerfile.dependencies
+
+    TRILINOS_SRC=${TRILINOS_SRC:=${SCRIPT_PATH}/trilinos/source}
+    [ ! -d "${TRILINOS_SRC}" ] && git clone --branch develop git@github.com:trilinos/Trilinos.git ${TRILINOS_SRC}
+
+    KOKKOS_TOOLS_SRC=${KOKKOS_TOOLS_SRC:=${SCRIPT_PATH}/kokkos-tools/source}
+    [ ! -d "${KOKKOS_TOOLS_SRC}" ] && git clone --branch develop git@github.com:kokkos/kokkos-tools.git ${KOKKOS_TOOLS_SRC}
+
+    PODMAN_ARGS+=(
+        "-v${TRILINOS_SRC}:${WORKSPACE}/trilinos/source"
+        "-v${TRILINOS_BUILD}:${WORKSPACE}/trilinos/build"
+        "-v${TRILINOS_INSTALL}:${WORKSPACE}/trilinos/install"
+        "-e TRILINOS_SRC_HOST=${TRILINOS_SRC}"
+        "-e TRILINOS_BUILD_HOST=${TRILINOS_BUILD}"
+        "-e TRILINOS_INSTALL_HOST=${TRILINOS_INSTALL}"
+
+        "-v${KOKKOS_TOOLS_SRC}:${WORKSPACE}/kokkos-tools/source"
+        "-v${KOKKOS_TOOLS_BUILD}:${WORKSPACE}/kokkos-tools/build"
+        "-v${KOKKOS_TOOLS_INSTALL}:${WORKSPACE}/kokkos-tools/install"
+        "-e KOKKOS_TOOLS_SRC_HOST=${KOKKOS_TOOLS_SRC}"
+        "-e KOKKOS_TOOLS_BUILD_HOST=${KOKKOS_TOOLS_BUILD}"
+        "-e KOKKOS_TOOLS_INSTALL_HOST=${KOKKOS_TOOLS_INSTALL}"
+    )
+
+elif [[ $CONFIG == "trilinos" ]] ; then
+    ##################################################
+    # container name
+    CONTAINER_NAME=pytrilinos2_trilinos
+
+    ##################################################
+    # image
+    CONTAINER_HOME=/root
+
+    IMAGE=ghcr.io/trilinos/pytrilinos2_container/trilinos:latest
+    DOCKERFILE=${SCRIPT_PATH}/Dockerfile.trilinos
+
+fi
+
+PODMAN_ARGS+=(
+    "-e OMP_NUM_THREADS=2"
+    "-v${SCRIPT_PATH}:/scripts:ro"
+)
+PODMAN_BUILD_ARGS=()
+LAUNCH_COMMANDS+=("source /scripts/commandsDevContainer.sh")
+
+##################################################
+# Trilinos
+
+TRILINOS_BUILD=${TRILINOS_BUILD:=${SCRIPT_PATH}/trilinos/build}
+TRILINOS_INSTALL=${TRILINOS_INSTALL:=${SCRIPT_PATH}/trilinos/install}
+
+mkdir -p ${TRILINOS_BUILD}
+mkdir -p ${TRILINOS_INSTALL}
+
+PODMAN_ARGS+=(
+    "-e TRILINOS_DIR=${WORKSPACE}/trilinos/source"
+    "-e TRILINOS_BUILD_DIR=${WORKSPACE}/trilinos/build"
+    "-e TRILINOS_INSTALL_DIR=${WORKSPACE}/trilinos/install"
+    "-e PYTHONPATH=${WORKSPACE}/trilinos/build/packages/PyTrilinos2"
+)
+
+LAUNCH_COMMANDS+=("cd \${TRILINOS_BUILD_DIR}")
+
+
+##################################################
+# Kokkos Tools
+
+KOKKOS_TOOLS_BUILD=${KOKKOS_TOOLS_BUILD:=${SCRIPT_PATH}/kokkos-tools/build}
+KOKKOS_TOOLS_INSTALL=${KOKKOS_TOOLS_INSTALL:=${SCRIPT_PATH}/kokkos-tools/install}
+
+mkdir -p ${KOKKOS_TOOLS_BUILD}
+mkdir -p ${KOKKOS_TOOLS_INSTALL}
+
+PODMAN_ARGS+=(
+    "-e KOKKOS_TOOLS_DIR=${WORKSPACE}/kokkos-tools/source"
+    "-e KOKKOS_TOOLS_BUILD_DIR=${WORKSPACE}/kokkos-tools/build"
+    "-e KOKKOS_TOOLS_INSTALL_DIR=${WORKSPACE}/kokkos-tools/install"
+)
+
+
+##################################################
+# Jupyter notebook server
+
+mkdir -p ${SCRIPT_PATH}/notebooks
+NOTEBOOK_PORT=9999
+PODMAN_ARGS+=(
+    "-e NOTEBOOK_PORT=${NOTEBOOK_PORT}"
+    "-p${NOTEBOOK_PORT}:${NOTEBOOK_PORT}"
+    "-v${SCRIPT_PATH}/notebooks:${WORKSPACE}/notebooks"
+)
+LAUNCH_COMMANDS+=(
+    "jupyter notebook --port=${NOTEBOOK_PORT} --no-browser --allow-root --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --notebook-dir=${WORKSPACE}/notebooks > /dev/null 2>&1 & echo 'Jupyter Notebook server started.'"
+)
